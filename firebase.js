928ffc3e87422bd177c1bfa64387ce71987f64f0
duya5082-coder <duya5082@gmail.com>
2026-07-27 14:17:30 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,50 +1,79 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
+
 import {
+
 getDatabase,
+
 ref,
+
 push,
+
 set,
+
 onValue,
+
 remove,
+
 update
+
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
 
 
 const firebaseConfig = {
 
+
 apiKey: "AIzaSyC5053lsBtGbC8rWll-8TRlAXf5lxENbPY",
+
 
 authDomain: "tta-custom-manager.firebaseapp.com",
 
+
 databaseURL: "https://tta-custom-manager-default-rtdb.asia-southeast1.firebasedatabase.app",
+
 
 projectId: "tta-custom-manager",
 
+
 storageBucket: "tta-custom-manager.firebasestorage.app",
 
+
 messagingSenderId: "1042103585902",
+
 
 appId: "1:1042103585902:web:d2fc07315553340d4eaa7f"
 
 };
 
 
+
 const app = initializeApp(firebaseConfig);
+
 
 
 const db = getDatabase(app);
 
 
-console.log("Firebase OK");
+
+console.log("🔥 FIREBASE CONNECTED");
+
 
 
 export {
+
 db,
+
 ref,
+
 push,
+
 set,
+
 onValue,
+
 remove,
+
 update
+
 };
