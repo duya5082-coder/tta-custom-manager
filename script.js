7be20440f55a9b1baac29189c3a450d3dc85bf56
duya5082-coder <duya5082@gmail.com>
2026-07-27 12:37:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+*{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+}
 
+body{
+    background:#0d1117;
+    color:#fff;
+    font-family:Arial, Helvetica, sans-serif;
+}
+
+header{
+    background:#161b22;
+    padding:20px;
+    text-align:center;
+    border-bottom:2px solid #00ff88;
+}
+
+header h1{
+    color:#00ff88;
+    font-size:32px;
+}
+
+#app{
+    max-width:1400px;
+    margin:auto;
+    padding:20px;
+}
