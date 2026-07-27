7440f621e5f0b3f6392a6a9b447b2708402068a2
duya5082-coder <duya5082@gmail.com>
2026-07-27 23:44:08 +0700

    Create notification.js

---
+++ notification.js
@@ -0,0 +1,84 @@
+// =====================================
+// CUSTOM TTA
+// NOTIFICATION
+// =====================================
+
+
+if(window.TTA_NOTIFICATION_LOADED){
+
+console.warn(
+"notification loaded"
+);
+
+
+}else{
+
+
+window.TTA_NOTIFICATION_LOADED=true;
+
+
+
+document.addEventListener(
+
+"DOMContentLoaded",
+
+()=>{
+
+
+console.log(
+"🔔 Notification ready"
+);
+
+
+
+});
+
+
+
+window.sendNotification=function(msg){
+
+
+
+const list=
+
+document.getElementById(
+"notificationList"
+);
+
+
+
+if(!list)
+
+return;
+
+
+
+const div=document.createElement("div");
+
+
+
+div.className="notificationItem";
+
+
+
+div.innerHTML=
+
+`
+🔔 ${msg}
+<br>
+<small>
+${new Date().toLocaleString()}
+</small>
+`;
+
+
+
+list.prepend(div);
+
+
+
+};
+
+
+
+}
